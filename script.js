dc6df55191172101582c01b335239afd75e0c00b
KirigakureSU <144735073+KirigakureSU@users.noreply.github.com>
2024-05-13 13:26:11 -0600

    Txt to text

--- script.js
+++ script.js
@@ -23,11 +23,10 @@ function comprarElemento(e) {
 
 function leerDatosElemento(elemento) {
     const infoElemento = {
-        imagen: elemento.querySelector('img').search,
-        titulo: elemento.querySelector('h3').txtContent,
-        precio: elemento.querySelector('.precio').txtContent,
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('.precio').textContent,
         id:elemento.querySelector('a').getAttribute('data-id')
-
     }
     insertarCarrito(infoElemento);
 }
